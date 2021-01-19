da8f04616bd5e69af5910b86b40b93b0eccb1fab
DavidRuvinskiy <david.ruvinskiy@sbcglobal.net>
2021-01-19 12:36:00 -0600

    Initial Commit

---
+++ SwiftUICodingChallengeRuvinskiy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICodingChallengeRuvinskiy
+//
+//  Created by David Ruvinskiy on 1/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICodingChallengeRuvinskiy/SwiftUICodingChallengeRuvinskiyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICodingChallengeRuvinskiyApp.swift
+//  SwiftUICodingChallengeRuvinskiy
+//
+//  Created by David Ruvinskiy on 1/19/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICodingChallengeRuvinskiyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
